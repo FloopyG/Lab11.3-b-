9d7f2eb53e19572f50594d9f9f3a7e5b580d20f2
FloopyG <36450035+FloopyG@users.noreply.github.com>
2020-12-24 14:06:44 +0200

    Initial commit

---
+++ UnitTest11.3(b)/UnitTest11.3(b).cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab11.3(b).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest113b
+{
+	TEST_CLASS(UnitTest113b)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char g[6] = "g.dat";
+			int t = binSearch(g, "1", 1, 1);
+			Assert::AreEqual(t, -1);
+		}
+	};
+}
